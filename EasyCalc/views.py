4152a0feb3a805f36223dc8cf4e393d88f9a4283
AdeyinkaEsi1 <yhamid2828@gmail.com>
2024-01-18 14:58:40 +0100

    added calculator function to handle POST request

--- EasyCalc/views.py
+++ EasyCalc/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .utils import mat_operation
 
-# Create your views here.
+
+def calculator(request):
+    result = None
+
+    if request.method == 'POST':
+        try:
+            inp1 = int(request.POST['input1: '])
+            operator = int(request.POST['operator: '])
+            inp2 = int(request.POST['input2: '])
+
+            result = mat_operation(inp1, operator, inp2)
+
+            if isinstance(result, (int, float)):
+                print(f'Result: {result}')
+            else:
+                result = f'Error: {result}'
+        except ValueError as ve:
+            result = f'Error: Invalid input. Please enter valid numbers. - {ve}'
+        except Exception as e:
+            result = f'Error: {e}'
+    
+
+    return render(request, 'EasyCalc/index.html', {'result': result})
+
+
+# if __name__ == "__main__":
+#     calculator()
+
