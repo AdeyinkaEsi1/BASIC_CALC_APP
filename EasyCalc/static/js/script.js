5dcfa7f454ceaaff9a2c2674bc0b37b33f084b65
AdeyinkaEsi1 <yhamid2828@gmail.com>
2024-01-22 19:51:27 +0100

    added js file

---
+++ EasyCalc/static/js/script.js
@@ -0,0 +1,22 @@
+let screen = document.getElementById('screen');
+
+function appendToScreen(value) {
+    if (screen.textContent === '0' || screen.textContent === 'Error') {
+        screen.textContent = value;
+    } else {
+        screen.textContent += value;
+    }
+}
+
+
+function clearScreen() {
+    screen.textContent = '0';
+}
+
+function calculate() {
+    try {
+        screen.textContent = eval(screen.textContent);
+    } catch (error) {
+        screen.textContent = 'Error';
+    }
+}
